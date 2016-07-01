8c81568422ec20783c69bdebb7a6020c70499c16
Patrick Allen <email": null>
2016-06-30 18:06:43 -0700

    add home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,22 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+devise_for :users
+# Run rake routes to see what this added.
+# DEVISE HELPERS WORTH NOTING:
+  # before_action :authenticate_user! (for CONTROLLER)
+  # user_signed_in? (Ruby/HTML.ERB)
+  # current_user (Ruby/HTML.ERB)
+  # user_session (Ruby/HTML.ERB)
+# To setup Devise:
+  # Gemfile: (+) gem 'devise', '~> 3.4.1' & gem 'responders'
+  # Console: bundle
+  # Console: rails generate devise:install
+  # Console: rails generate devise MODEL (MODEL = User, or Admin, or Member, etc.)
+  # Routes.rb: config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  ### THE ABOVE IS A PLACEHOLDER FOR EMAIL VERIFICATION SENDS ###
+  # Routes.rb: Add some kind of root_path. Devise will rely on this sometimes. 
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-end
+root to: "home#index" 
+# This is a placeholder. Devise requires a root path as an option.
+# Update to proper static homepage when available.
+end  
