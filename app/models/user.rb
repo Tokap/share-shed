458ea6d8b0d27353e4b9d3fab1ca0b053967cdf1
Patrick Allen <allenpatrick86@gmail.com>
2016-07-01 12:52:09 -0700

    Complete user model associations and added method to sum rented_tools for renter

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
 
   has_many :rented_rentals, class_name: :Rental, foreign_key: :renter_id
   has_many :rented_line_items, through: :rented_rentals, source: :line_items
-  has_many :rented_tools, through: :rented_line_items, source: :tools
+  has_many :rented_tools, through: :rented_line_items, source: :tool
 
  
 
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
 # Once logic is implimented to determine the duration of the rental,
 # a multiplyer should be added
 
-  def sum_all_tools
+  def owner_sum_all_tools
     tool_prices = []
     tools.each do |tool|
       tool_prices << tool.base_price
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
     tool_prices.reduce(:+)
   end
 
-  def sum_rented_tools
+  def owner_sum_rented_tools
     tool_prices = []
     tools.each do |tool|
       tool_prices << tool.base_price if tool.available == false
@@ -37,10 +37,18 @@ class User < ActiveRecord::Base
     tool_prices.reduce(:+)
   end
 
-  def sum_inactive_tools
+  def owner_sum_inactive_tools
     tool_prices = []
     tools.each do |tool|
       tool_prices << tool.base_price if tool.available == true
+    end
+    tool_prices.reduce(:+)
+  end
+
+  def renter_sum_rented_tools
+    tool_prices = []
+    rented_tools.each do |tool|
+      tool_prices << tool.base_price
     end
     tool_prices.reduce(:+)
   end
