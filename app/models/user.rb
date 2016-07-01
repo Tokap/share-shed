01cdb7672e179d7983e5899a8aeab973c1f741e7
Patrick Allen <allenpatrick86@gmail.com>
2016-06-30 22:51:23 -0700

    add notes concerning sum functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ActiveRecord::Base
   has_many :owned_rentals, foreign_key: :owner_id
   # has_many :line_items, through: :rentals
 
+ 
+
+# These sums currently work as if the base_price is a flat rate.
+# Once logic is implimented to determine the duration of the rental,
+# a multiplyer should be added
+
   def sum_all_tools
     tool_prices = []
     tools.each do |tool|
