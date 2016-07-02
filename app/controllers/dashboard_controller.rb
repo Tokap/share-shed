bad3f8929b90271772a5f89464c3d2bad5186c09
Elliot Wolfe <email": null>
2016-07-02 15:21:02 -0700

    added rental request links to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,8 @@ class DashboardController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-
+    @owned_rentals = @user.owned_rentals
+    @rented_rentals = @user.rented_rentals
   end
 
   def index
