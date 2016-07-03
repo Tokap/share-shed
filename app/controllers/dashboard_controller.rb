ad3f3111a962098c43d2b90ea325721bd54358c8
Patrick Allen <email": null>
2016-07-02 17:33:03 -0700

    add controller logic to prevent users from viewing eachother's dashboards

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,11 @@ class DashboardController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-    @rental = Rental.where(user: @user)
+  	if @user == current_user
+    	@renter_rentals = Rental.where(user: @user)
+    else
+    	redirect_to root_path
+    end
   end
 
   def index
