38089a4e589a0a3004dbb1c7a465f6e9852a6d48
Patrick Allen <email": null>
2016-07-02 17:51:59 -0700

    Clean up date/time

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,8 @@ class DashboardController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	if @user == current_user
-    	@renter_rentals = Rental.where(user: @user)
+    	@rented_rentals = Rental.where(renter: @user).order('status DESC')
+    	#rentals ordered by status
     else
     	redirect_to root_path
     end
