d774249860bead1955ad79682a3a044c8537e3a4
Patrick Allen <email": null>
2016-07-04 15:49:59 -0700

    refine tagging process to disallow duplicate tags on same tool

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,15 +8,11 @@ class TagsController < ApplicationController
 		# creates tag new tag if it doesn't exist
 		@tool = Tool.find(params[:tool])
 		tag_names = params[:tag][:name].split(/\s*,\s*/)
-	 tag_names.each do |tag_name|
-	 	 tag = Tag.find_by(name: tag_name.downcase)
-	 	 if tag.nil?
-		   tag = Tag.create(name: tag_name.downcase)
-		   ToolTag.create(tag: tag, tool: @tool)
-		 else
-		   ToolTag.create(tag: tag, tool: @tool)
-		 end
-	 end
+	 	tag_names.each do |tag_name|
+	 	 tag = Tag.find_or_create_by(name: tag_name.downcase)
+		 ToolTag.find_or_create_by(tag: tag, tool: @tool)
+		end
+
 	 redirect_to(@tool) 
 	end
 
