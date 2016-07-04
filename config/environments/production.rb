29b1d57292f83964c8255d22bb299a4aff6d47fc
Mike Laurel <mlaurel@gmail.com>
2016-07-03 21:41:43 -0700

    mailgun WORKING, adding domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
       :address        => ENV['MAILGUN_SMTP_SERVER'],
       :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
       :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-      :domain         => 'yourapp.heroku.com',
+      :domain         => ENV['MAILGUN_DOMAIN'],
       :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
