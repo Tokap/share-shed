9ca2b78dd20f5b56daa3657676cce03e11fb6548
Patrick Allen <email": null>
2016-07-04 10:12:50 -0700

    add helper method so we can call name on tool and get the abstract_tool.name in one shot

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -11,4 +11,8 @@ class Tool < ActiveRecord::Base
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+  def name
+  	abstract_tool.name
+  end
+
 end
