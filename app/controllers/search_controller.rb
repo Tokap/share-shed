d1b4223d2fdfacffec5feb74100a0424127f514b
Patrick Allen <email": null>
2016-07-04 15:36:16 -0700

    impliment search feature for tags or tool names

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,23 @@
 class SearchController < ApplicationController
 
 	def index
-		@tag = Tag.find_by(name: params[:name])
-		@tool_tags = ToolTag.where(tag: @tag) 
+		if params[:type] == "tag"
+			@tag = Tag.find_by(name: params[:name])
+			@results = ToolTag.where(tag: @tag)
+		else
+			@results = []
+			name = "%#{params[:name]}%"
+			@atools = AbstractTool.where("name like ?", name)
+			if !@atools.nil?
+				@atools.each do |atool| 
+					atool.tools.each do |tool_object|
+						@results << tool_object
+					end
+				end
+			else	
+				@results = nil
+			end
+		end 
 	end
 
 end
