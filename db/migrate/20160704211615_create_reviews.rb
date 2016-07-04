7be2bdc38157b75dce62518448cf2ec2ed5edfcf
Elliot Wolfe <email": null>
2016-07-04 14:27:19 -0700

    create reviews migration

---
+++ db/migrate/20160704211615_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :reviewer_id, null: false
+      t.integer :reviewee_id, null: false
+      t.integer :rating, null: false
+      t.text :content
+
+      t.timestamps(null: false)
+    end
+  end
+end
