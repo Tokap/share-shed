a43462123ff36bb080cf36f156d5b80bd06e1b1a
Patrick Allen <allenpatrick86@gmail.com>
2016-07-05 23:37:35 -0700

    minor revisions to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -292,13 +292,15 @@ LineItem.create!(tool:    demo_user_tool_5,
 # create extra tools to populate search database
 dummmy_users = [user_2, user_3, user_4, user_5, user_6, user_7, user_8, user_9, user_10, user_11, user_12, user_13, user_14, user_15, user_16, user_17, user_18, user_19]
 
-dummmy_users.each do |user|
-  Tool.create!(abstract_tool_id:  rand(1..20),
-               base_price:        rand(5..50),
-               available:         true,
-               owner:             user,
-               model_number:      Faker::Company.ein,
-               description:       Faker::Hipster.paragraph)
+7.times do
+  dummmy_users.each do |user|
+    Tool.create!(abstract_tool_id:  rand(1..20),
+                 base_price:        rand(5..30),
+                 available:         true,
+                 owner:             user,
+                 model_number:      Faker::Company.ein,
+                 description:       Faker::Hipster.paragraph)
+  end
 end
 
 # add a log at the appropriate interval
