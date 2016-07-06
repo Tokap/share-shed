6f8f01ec4b1c9ce7aee4f87c3dc6155f2a61b105
Mike Laurel <mlaurel@gmail.com>
2016-07-05 20:28:23 -0700

    adding svg with css class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
   # def devise_mapping
   #   @devise_mapping ||= Devise.mappings[:user]
   # end
+
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/svg/#{name}.svg"
+    puts "\n\n\n\n\nfile path"
+    puts file_path
+    if File.exists?(file_path)
+      puts "\n\ninif\n\n"
+      File.read(file_path).html_safe
+    else
+      '(not found)'
+    end
+  end
 end
