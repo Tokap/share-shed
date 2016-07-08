cf7f11202b6b1f46d322b80c06e1ce9d215d3d8e
Patrick Allen <email": null>
2016-07-07 20:42:08 -0700

    update seed file and resolve problems

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -8,7 +8,7 @@ class Tool < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   belongs_to :owner, class_name: "User"
 
-  has_many :tool_tags
+  has_many :tool_tags, dependent: :destroy
   has_many :tags, through: :tool_tags
 
   has_attached_file :image,
